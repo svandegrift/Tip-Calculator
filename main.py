e976c82b591199308071d10e4abc40d40991c45c
Steven Vandegrift <svandegrift@icloud.com>
2021-12-02 12:48:05 -0600

    Update Format
    Format now updated to display currency properly

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ percent = int(input("What percentage tip would you like to give? 10, 12, or 15?\
 people = int(input("How many people to split the bill?\n"))
 
 result = round((total + (total * percent)) / people, 2)
-
+result = "{:.2f}".format(result)
 print(f"Each person should pay ${result}")
